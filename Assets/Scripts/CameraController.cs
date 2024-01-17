b9a37a9280f2f78c2993436d286019cf4249b471
FolkAkaranan <69294102+FolkAkaranan@users.noreply.github.com>
2024-01-17 12:56:40 +0700

    code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Camera cam;
+
+    [Header("Move")]
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private float xInput;
+    [SerializeField] private float zInput;
+
+    public static CameraController instance;
+
+    private void MoveByKB()
     {
-        
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("vertical");
+
+        Vector3 dir = (transform.forward * zInput) + (transform.right * xInput);
+
+        transform.position += dir * moveSpeed * Time.deltaTime;
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        cam = Camera.main;
     }
 
     // Update is called once per frame
+    void Start()
+    {
+        moveSpeed = 50;
+    }
+
     void Update()
     {
-        
+        MoveByKB();
     }
 }
