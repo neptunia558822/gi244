83818b6d3e253a194dfa8f2c267301860d3692c7
neptunia558822 <neptunia558822@gmail.com>
2024-01-24 13:01:41 +0700

    ทำunitเเล้ว

---
+++ Assets/Scripts/Unit/Unit.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum UnitState
+{
+    Idle,
+    Move,
+    Attack,
+    Die
+}
+
+public class Unit : MonoBehaviour
+{
+    [SerializeField] private int id;
+    public int ID {  get { return id; } set { id = value; } }
+
+    [SerializeField] private string unitName;
+    public string UnitName { get { return unitName;} }
+
+    [SerializeField] private Sprite unitPic;
+    public Sprite UnitPic { get { return unitPic;} }
+
+    [SerializeField] private int curHP;
+    public int CurHP { get { return curHP; } set { curHP = value; } }
+
+    [SerializeField] private int maxHP = 100;
+    public int MaxHP { get { return maxHP;} }
+
+    [SerializeField] private int moveSpeed = 5;
+    public int MoveSpeed { get {  return moveSpeed; } }
+
+    [SerializeField] private int minWpnDamage;
+    public int MinWpnDamage { get {  return minWpnDamage; } }
+
+    [SerializeField] private int maxWpnDamage;
+    public int MaxWpnDamage { get { return maxWpnDamage; } }
+
+    [SerializeField] private int armour;
+    public int Armour { get {  return armour; } }
+
+    [SerializeField] private float visualRange;
+    public float VisualRange { get {  return visualRange; } }
+
+       void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
