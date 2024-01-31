0d944c99801095c5c36731fa1f773c566a8cc6d4
neptunia558822 <neptunia558822@gmail.com>
2024-01-31 18:24:09 +0700

    ลองเทสการสั่งการ Unit ด้วยเมาส์ขวา

---
+++ Assets/Scripts/Command/UnitCommand.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitCommand : MonoBehaviour
+{
+    public LayerMask layerMask;
+    private UnitSelect unitSelect;
+
+    private Camera cam;
+
+    private void UnitsMoveToPosition(Vector3 dest, Unit unit)
+    {
+        if (unit != null)
+            unit.MoveToPosition(dest);
+    }
+
+    private void CommandToGround(RaycastHit hit, Unit unit)
+    {
+        UnitsMoveToPosition(hit.point, unit);
+    }
+
+    private void TryCommand(Vector2 screenPos)
+    {
+        Ray ray = cam.ScreenPointToRay(screenPos);
+        RaycastHit hit;
+
+        //if we left-click something
+        if (Physics.Raycast(ray, out hit, 1000, layerMask))
+        {
+            switch (hit.collider.tag)
+            {
+                case "Ground":
+                    CommandToGround(hit, unitSelect.CurUnit);
+                    break;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+
+        layerMask = LayerMask.GetMask("Unit", "Building", "Resource", "Ground");
+    }
+
+    void Awake()
+    {
+        unitSelect = GetComponent<UnitSelect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(1))
+        {
+            TryCommand(Input.mousePosition);
+        }
+    }
+}
