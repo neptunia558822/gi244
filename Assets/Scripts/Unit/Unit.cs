ed3fbbe50369c9806565f20b2176c6b07dc9a601
neptunia558822 <neptunia558822@gmail.com>
2024-01-31 18:20:05 +0700

    การสั่งการ Unit ด้วยเมาส์ขวา

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -63,7 +63,37 @@ public class Unit : MonoBehaviour
             selectionVisual.SetActive(flag);
     }
 
-       void Start()
+    public void SetState(UnitState toState)
+    {
+        state = toState;
+
+        if (state == UnitState.Idle)
+        {
+            navAgent.isStopped = true;
+            navAgent.ResetPath();
+        }
+    }
+
+    public void MoveToPosition(Vector3 dest)
+    {
+        if (navAgent != null)
+        {
+            navAgent.SetDestination(dest);
+            navAgent.isStopped = false;
+        }
+
+        SetState(UnitState.Move);
+    }
+
+    private void MoveUpdate()
+    {
+        float distance = Vector3.Distance(transform.position, navAgent.destination);
+
+        if (distance <= 1f)
+            SetState(UnitState.Idle);
+    }
+
+    void Start()
     {
         
     }
@@ -71,7 +101,13 @@ public class Unit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (state)
+        {
+            case UnitState.Move:
+                MoveUpdate();
+                break;
+        }
+
     }
 
     void Awake()
