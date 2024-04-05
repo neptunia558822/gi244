6218704323f38357b5297a333df38222f06899aa
neptunia558822 <neptunia558822@gmail.com>
2024-04-05 18:37:04 +0700

    Week12 เสร็จละ

---
+++ Assets/Scripts/SelectMap.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectMap : MonoBehaviour
+{
+    [SerializeField] private string mapScene;
+
+    public void ChooseMap(string mapName)
+    {
+        mapScene = mapName;
+    }
+
+    public void StartGame()
+    {
+        if (mapScene == "")
+            return;
+
+        Settings.currentScene = mapScene;
+        SceneManager.LoadScene(mapScene);
+    }
+}
